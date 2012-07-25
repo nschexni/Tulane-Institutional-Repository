32aeea6dabb3ac6e3b9242da8da6cc41bf92dba6
Neal Schexnider <nschexni@tulane.edu>
2012-07-25 14:07:05 -0500

    getting ready to merge with homepage

---
+++ application/views/common/main_nav_two_level.php
@@ -0,0 +1,58 @@
+
+<!--//link to the CSS files for this menu type -->
+<link rel="stylesheet" media="screen" href="<?=base_url()?>assets/css/superfish.css" /> 
+<link rel="stylesheet" media="screen" href="<?=base_url()?>assets/css/superfish-navbar.css" /> 
+ 
+<!--// link to the JavaScript files (hoverIntent is optional) -->
+<script src="<?=base_url()?>assets/js/custom/jquery_hoverIntent.js"></script> 
+<script src="<?=base_url()?>assets/js/custom/jquery_superfish.js"></script> 
+ 
+<!--// initialise Superfish -->
+<script> 
+ 
+    $(document).ready(function(){ 
+        $("ul.sf-menu").superfish({ 
+            pathClass:  'current' 
+        }); 
+    }); 
+ 
+</script>
+
+
+    <ul class="sf-menu sf-navbar">
+            <li>
+                <a href="<?= base_url();?>">about</a>
+            </li>
+            <li>
+                <a href="http://digitallibrary.tulane.edu">digital library</a>
+                    <ul>
+                        <li><a href="http://digitallibrary.tulane.edu">about</a></li>
+                        <li><a href="http://digitallibrary.tulane.edu">refine by</a></li>
+                        <li><a href="http://digitallibrary.tulane.edu">full list</a></li>
+            </li>
+                            <li>
+                                    <a href="#">menu item</a>
+                                    <ul>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                    </ul>
+                            </li>
+                            <li>
+                                    <a href="#">menu item</a>
+                                    <ul>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                            <li><a href="#">menu item</a></li>
+                                    </ul>
+                            </li>
+                    </ul>
+            </li>
+            <li>
+                    <a href="#">menu item</a>
+            </li>	
+    </ul>
